a1c0100b9893dac3d61288da5fa952b59307e71a
medsaiefbardi <bardimohamedsaief@gmail.com>
2021-07-04 12:17:30 +0100

    errors :(

--- service/server/apiService/api.js
+++ service/server/apiService/api.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const {token,apii} = require('./token');
+const {token,apii} = require('../token');
 const api = axios.create({
   baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc',
   headers: {
